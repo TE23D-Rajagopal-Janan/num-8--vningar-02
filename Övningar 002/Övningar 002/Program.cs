4a8264b590b988bdcb3aefa211d68831bd2cc5ce
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-11-29 10:55:11 +0100

    lksv

---
+++ Övningar 002/Övningar 002/Program.cs
@@ -0,0 +1,22 @@
+﻿//ÖVNING 1 
+string[] leksaker = {"leksak1", "leksak2", "leksak3", "leksak4", "leksak5"};
+// Console.WriteLine(string.Join("\n", leksaker));
+
+// ÖVNING 2 
+
+foreach (string lek in leksaker)
+{
+    Console.WriteLine(lek);
+}
+
+string[] namn = {"Martin", "Matija", "Imad", "Adam", "Saymmon"};
+
+int[] num = {2, 3, 5, 7, 9};
+// string numm;
+// int.TryParse(num, out num)
+foreach (string sentence in num; string lek2 in leksaker; string nam in namn )
+{
+    Console.WriteLine($""name" ger "num" "leksaker" till "name");
+}
+Console.ReadLine();
+
